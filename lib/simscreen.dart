c870867d8f4af62287e6f06d9fdd09316f07ba53
Jonathan <57756132+jdk-21@users.noreply.github.com>
2021-04-12 15:19:28 +0200

    adds quartz and runtime ui

--- lib/simscreen.dart
+++ lib/simscreen.dart
@@ -32,14 +32,17 @@ class _SimScreenState extends State<SimScreen> {
       });
       await Future.delayed(const Duration(milliseconds: 100));
     }
+    setState(() {
+      program[lastIndex]['isSelected'] = false;
+    });
     return;
   }
 
   @override
   Widget build(BuildContext context) {
+    // shows dialog to change the storage
     createStorageDialog(BuildContext context, int index) {
       var txt = TextEditingController();
-
       return showDialog(
           context: context,
           builder: (context) {
@@ -79,6 +82,24 @@ class _SimScreenState extends State<SimScreen> {
           });
     }
 
+    changeTrisBit(int bit, int index) {
+      String temp = "00000000" +
+          int.parse(storage.value[index], radix: 16).toRadixString(2);
+      temp = temp.substring(temp.length - 8);
+      switch (int.parse(temp[bit])) {
+        case 0:
+          temp = temp.substring(0, bit) + "1" + temp.substring(bit + 1);
+          break;
+        case 1:
+          temp = temp.substring(0, bit) + "0" + temp.substring(bit + 1);
+          break;
+      }
+      temp = "00" + int.parse(temp, radix: 2).toRadixString(16);
+      setState(() {
+        storage.value[index] = temp.substring(temp.length - 2);
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("PicSim"),
@@ -114,7 +135,6 @@ class _SimScreenState extends State<SimScreen> {
                             } else {
                               return InkWell(
                                 onTap: () {
-                                  print("hello");
                                   createStorageDialog(context, index - row - 1);
                                 },
                                 child: Container(
@@ -128,33 +148,159 @@ class _SimScreenState extends State<SimScreen> {
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  width: 300,
+                  height: 200,
+                  child: ValueListenableBuilder(
+                    valueListenable: storage,
+                    builder: (context, value, child) {
+                      return GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 9),
+                          itemCount: 135,
+                          itemBuilder: (context, index) {
+                            // row calculates the current row
+                            var row = index ~/ 9;
+                            var register = row ~/ 3;
+                            var bit = index % 9 - 1;
+                            // checks if this is a line start
+                            String name = "";
+                            if (index % 9 == 0) {
+                              switch (row % 3) {
+                                case 0:
+                                  switch (register) {
+                                    case 0:
+                                      name = "RA";
+                                      break;
+                                    case 1:
+                                      name = "RB";
+                                      break;
+                                    case 2:
+                                      name = "RC";
+                                      break;
+                                    case 3:
+                                      name = "RD";
+                                      break;
+                                    case 4:
+                                      name = "RE";
+                                      break;
+                                  }
+                                  break;
+                                case 1:
+                                  name = "Tris";
+                                  break;
+
+                                case 2:
+                                  name = "Pin";
+                                  break;
+                              }
+
+                              return Container(
+                                color: Colors.amber,
+                                child: Center(child: Text(name)),
+                              );
+                            } else {
+                              switch (row % 3) {
+                                case 0:
+                                  name = (7 - bit).toString();
+                                  break;
+                                case 1:
+                                  name = (7 - bit).toString();
+                                  break;
+                                case 2:
+                                  String temp = "00000000" +
+                                      int.parse(storage.value[register + 5],
+                                              radix: 16)
+                                          .toRadixString(2);
+                                  name = temp.substring(temp.length - 8)[bit];
+                                  return InkWell(
+                                    onTap: () {
+                                      changeTrisBit(bit, register + 5);
+                                    },
+                                    child: Container(
+                                        decoration: BoxDecoration(
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  color: Colors.grey,
+                                                  width: 2)),
+                                          color: Colors.blue[100],
+                                        ),
+                                        child: Center(child: Text(name))),
+                                  );
+                              }
+                              return Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[100],
+                                  ),
+                                  child: Center(child: Text(name)));
+                            }
+                          });
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
           Container(
             decoration: BoxDecoration(color: Colors.blue[50]),
-            child: ButtonBar(
-              alignment: MainAxisAlignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                OutlinedButton(
-                    onPressed: () {
-                      if (cycler.run)
-                        cycler.pause();
-                      else {
-                        cycler.start();
-                        highlighter();
-                      }
-                    },
-                    child: Text("Start"),
-                    style: OutlinedButton.styleFrom(
-                        primary: Colors.white, backgroundColor: Colors.green)),
-                OutlinedButton(
-                    onPressed: () => cycler.step(), child: Text("Step")),
-                OutlinedButton(
-                  onPressed: () => cycler.reset(),
-                  child: Text("Reset"),
-                  style: OutlinedButton.styleFrom(
-                      primary: Colors.white, backgroundColor: Colors.red),
-                )
+                Expanded(
+                  flex: 2,
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {
+                            if (cycler.run)
+                              cycler.pause();
+                            else {
+                              cycler.start();
+                              highlighter();
+                            }
+                          },
+                          child: Text("Start"),
+                          style: OutlinedButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: Colors.green)),
+                      OutlinedButton(
+                          onPressed: () => cycler.step(), child: Text("Step")),
+                      OutlinedButton(
+                        onPressed: () => cycler.reset(),
+                        child: Text("Reset"),
+                        style: OutlinedButton.styleFrom(
+                            primary: Colors.white, backgroundColor: Colors.red),
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Quartz: " + "4,000000" + " MHz (" + "1.000" + " µs)",
+                    ),
+                    Row(
+                      children: [
+                        Text("Runtime: " + "00:13" + " µs"),
+                        Tooltip(
+                            message: "Clear runtime counter",
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.clear,
+                                size: 15,
+                              ),
+                              color: Colors.red[600],
+                              hoverColor: Colors.transparent,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
